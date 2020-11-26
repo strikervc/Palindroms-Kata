0b7ecd6d37f0d4df61908853245e32851a3a41a3
Victor Valdez <victor334_@hotmail.com>
2020-11-26 15:39:52 -0400

    test case added correctly

--- palindroms.test.js
+++ palindroms.test.js
@@ -1,9 +1,12 @@
 const palindrom = require('./palindroms')
 
 test('identifies correctly a palindrom', () =>{
-    var text = "oso"
-    var anothertext = "animal"
+    var text = "oso";  
     expect(palindrom(text)).toEqual("oso es un palíndromo")
+
+})
+
+test('identifies correctly a non-palindrom word', ()=>{
+    var anothertext = "animal";
     expect(palindrom(anothertext)).toEqual("animal no es un palíndromo")
- 
 })
