bf8d3fdb3d31c6536045caf34406a66df3021953
Victor Valdez <victor334_@hotmail.com>
2020-11-26 15:36:37 -0400

    logical solution developed and wordlist added

--- palindroms.test.js
+++ palindroms.test.js
@@ -1,8 +1,9 @@
-const { test } = require('@jest/globals')
 const palindrom = require('./palindroms')
 
 test('identifies correctly a palindrom', () =>{
     var text = "oso"
+    var anothertext = "animal"
     expect(palindrom(text)).toEqual("oso es un palíndromo")
+    expect(palindrom(anothertext)).toEqual("animal no es un palíndromo")
  
 })
