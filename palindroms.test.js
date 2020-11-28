372bb35f36e8900b049c16b64deb68914567ba4c
Victor Valdez <victor334_@hotmail.com>
2020-11-27 21:20:48 -0400

    new test added: identifies correctly a non-palindrom word

--- palindroms.test.js
+++ palindroms.test.js
@@ -4,3 +4,8 @@ test("identifies a palindrom word correctly", ()=>{
     var text = "arra"
     expect(palindrom(text)).toEqual("arra es un palíndromo")
 })
+
+test("identifies a non-palindrom word correctly", ()=>{
+    var atext = "rra"
+    expect(palindrom(atext)).toEqual("rra no un palíndromo")
+})
