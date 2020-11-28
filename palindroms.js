5a699bbe8080274b1b7b7e2982b9a445dd350916
Victor Valdez <victor334_@hotmail.com>
2020-11-28 07:56:56 -0400

    implementation of palindrom identification method incorporated

--- palindroms.js
+++ palindroms.js
@@ -1,7 +1,18 @@
+var fs = require('fs')
+var readline = require('readline')
+
 function isPalindrom(word){
     let stringChain = word.split("")
     let reversedChain = stringChain.reverse();
     return word==reversedChain.join("") ? word + " es un palíndromo" : word + " no es un palíndromo"
 }
+
+var reader = readline.createInterface({
+    input: fs.createReadStream('wordlist.txt')
+})
+
+reader.on('line', function(line){
+    console.log(isPalindrom(line))
+})
 
 module.exports = isPalindrom
