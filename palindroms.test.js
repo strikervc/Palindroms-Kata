498ead2916e7e5cced3d42c5520754f3cf1dd4c6
Victor Valdez <victor334_@hotmail.com>
2020-11-28 07:52:34 -0400

    non-palindrom word test added to test file

--- palindroms.test.js
+++ palindroms.test.js
@@ -1,6 +1,12 @@
 const palindrom = require('./palindroms')
 
 test("identifies correctly a palindrom word", ()=>{
+    var text = "ada"
+    expect(palindrom(text)).toEqual("ada es un palíndromo")
+})
+
+
+test("identifies correctly a non-palindrom word", ()=>{
     var text = "nada"
-    expect(palindrom(text)).toEqual("nada es un palíndromo")
+    expect(palindrom(text)).toEqual("nada no es un palíndromo")
 })
